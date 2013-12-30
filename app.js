6939e8e7fc89ff8ca37ce7f6b2b9270a948a0ddb
Brandon Sprague <brandon00sprague@gmail.com>
2013-12-30 00:38:32 -0500

    Favicon
    (And still testing deploy)

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ function hex_distance(coor1, coor2){
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
+app.use(express.favicon(path.join(__dirname, 'public/images/favicon.ico')));
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
